cb5511d6141f7801378020aacfed790a28efe67d
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-09-21 18:37:59 -0300

    Requisito 1 completo - Teste do product.py

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto_teste = Product(
+        1,
+        "celular",
+        "Samsung",
+        "24/11/1993",
+        "24/11/2023",
+        111,
+        "longe da água",
+    )
+
+    assert produto_teste.id == 1
+    assert produto_teste.nome_do_produto == 'celular'
+    assert produto_teste.nome_da_empresa == 'Samsung'
+    assert produto_teste.data_de_fabricacao == '24/11/1993'
+    assert produto_teste.data_de_validade == '24/11/2023'
+    assert produto_teste.numero_de_serie == 111
+    assert produto_teste.instrucoes_de_armazenamento == 'longe da água'
