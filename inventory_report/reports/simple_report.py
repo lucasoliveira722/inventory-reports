5ccb5db0f5a9903b7d124022d1ab9ce840fab0d7
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-09-23 19:13:55 -0300

    Requisito 3 completo - simple report

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,34 @@
+from datetime import date
+from collections import Counter
+
+
+class SimpleReport:
+
+    # Recebe uma lista de dicionários, retorna uma string do formato:
+    # Data de fabricação mais antiga: YYYY-MM-DD
+    # Data de validade mais próxima: YYYY-MM-DD
+    # Empresa com mais produtos: NOME DA EMPRESA
+
+    @staticmethod
+    def generate(list):
+        companies_names = []
+        oldest_fabrication_date = []
+        closest_expiration_date = []
+        # https://stackabuse.com/how-to-format-dates-in-python/
+        today = date.today().strftime("%Y-%m-%d")
+        for item in list:
+            # primeiro: empresa com mais produtos
+            companies_names.append(item["nome_da_empresa"])
+            most_common_company = Counter(companies_names).most_common(1)[0][0]
+
+            # segundo: data de fabricação
+            oldest_fabrication_date.append(item["data_de_fabricacao"])
+
+            # terceiro: data de validade
+            if (item["data_de_validade"] >= today):
+                closest_expiration_date.append(item["data_de_validade"])
+        return (
+            f"Data de fabricação mais antiga: {min(oldest_fabrication_date)}\n"
+            f"Data de validade mais próxima: {min(closest_expiration_date)}\n"
+            f"Empresa com mais produtos: {most_common_company}"
+        )
